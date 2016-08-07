b1af672014a142215bd2edd5034a65af6dc20623
lianzhao <knife.fe@gmail.com>
2016-08-08 00:42:52 +0800

    bug fix

--- src/content_script.js
+++ src/content_script.js
@@ -3,7 +3,8 @@
         evt.preventDefault();
         var url = evt.target.href;
         console.log(url);
-        var newurl = url.replace(/huiji.wiki\/wiki\/(%|\w|)*(\?)?/, "$1?action=render&");
+        var matches = url.match(/huiji.wiki\/wiki\/(%|\w|)*(\?)?/);
+        var newurl = url.replace(matches[0], matches[0]+"?action=render&");
         console.log(newurl);
         window.location.href = newurl;
     });
